f5d86092c992f56c62f39008e931b25f6395cdfb
Robin A. Meade <meader@hawaii.edu>
2025-01-30 09:05:41 -1000

    Indent

--- demo/intlib.c
+++ demo/intlib.c
@@ -2,9 +2,9 @@
 #include "intlib.h"
 
 /*
-    GETINTS: prompts for and returns 2 ints
-    return value of 0 means both ints sucessfully gotten from kbd
-    any other value indicates an error. return vals not to be used
+GETINTS: prompts for and returns 2 ints
+return value of 0 means both ints sucessfully gotten from kbd
+any other value indicates an error. return vals not to be used
 */
 int getInts(int *a, int *b)
 {
@@ -15,7 +15,7 @@ int getInts(int *a, int *b)
 
 
 /*
-    SWAPINTS: uses pointers to exchange values of incoming args
+SWAPINTS: uses pointers to exchange values of incoming args
 */
 void swapInts(int *a, int *b)
 {
@@ -26,7 +26,7 @@ void swapInts(int *a, int *b)
 
 
 /*
-    PRINT: takes copy args - no need to modify args
+PRINT: takes copy args - no need to modify args
 */
 void printInts(int a, int b)
 {
